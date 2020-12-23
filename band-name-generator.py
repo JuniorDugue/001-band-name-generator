510bbed8ea7ab317c83507fba26e66cde4b945ce
juniordugue <jrdugue@gmail.com>
2020-12-23 13:33:31 -0500

    reformat the py file so the code are under each objective

--- band-name-generator.py
+++ band-name-generator.py
@@ -1,16 +1,15 @@
 #1. Create a greeting for your program.
+print("welcome to the best band name generator!")
 
 #2. Ask the user for the city that they grew up in.
+city_name = input("what's the name of the city you grew up in? ")
 
 #3. Ask the user for the name of a pet.
+pet_name = input("what's the name of your last pet? ")
 
 #4. Combine the name of their city and pet and show them their band name.
+print("ladies & gentleman... give it up! for the " + city_name + " " + pet_name)
 
 #5. Make sure the input cursor shows on a new line, see the example at:
 #   https://band-name-generator-end.appbrewery.repl.run/
 
-print("welcome to the best band name generator!")
-city_name = input("what's the name of the city you grew up in? ")
-pet_name = input("what's the name of your last pet? ")
-
-print("ladies & gentleman... give it up! for the " + city_name + " " + pet_name)
